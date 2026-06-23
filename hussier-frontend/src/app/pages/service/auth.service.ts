65ce80dbb39c72db72e4afa4f126607629db48ff
Mickael490 <Abdoulrach2022@gmail.com>
2026-06-23 13:26:57 +0000

    fix: auth.service.ts URL backend production

--- hussier-frontend/src/app/pages/service/auth.service.ts
+++ hussier-frontend/src/app/pages/service/auth.service.ts
@@ -7,7 +7,7 @@ import { tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000/api/v1';
+  private apiUrl = 'https://huissier-backend.onrender.com/api/v1';
 
   constructor(private http: HttpClient, private router: Router) {}
 
